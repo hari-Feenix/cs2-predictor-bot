c77fb62c5926b78768b1f22ab2eb589d644f8052
Hari Feenix <hari@feenixgroup.com>
2025-05-18 00:27:18 +0100

    Add !results command to display recent match results

--- core/hltv_scraper.py
+++ core/hltv_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 def get_recent_results():
-    url = "https://www.hltv.org/results"
+    url = "https://bo3.gg/matches/finished"
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, "html.parser")
 
@@ -27,7 +27,7 @@ def get_recent_results():
 
 
 def get_upcoming_matches():
-    url = "https://www.hltv.org/matches"
+    url = "https://bo3.gg/matches/current"
     headers = {
         "User-Agent": "Mozilla/5.0"
     }
